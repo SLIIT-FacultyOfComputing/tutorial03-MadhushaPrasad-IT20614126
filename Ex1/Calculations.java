6d9ad0024f3ca8bcc2788279b20e5f890516f609
MadhushaPrasad <madushaprasad21@gmail.com>
2021-08-10 15:33:24 +0530

    exercise 01 done

---
+++ Ex1/Calculations.java
@@ -0,0 +1,12 @@
+class Calculations{
+
+    //create static method to add two numbers
+    public static void additionOfTwoNumbers(int num1 , int num2){
+        System.out.println("Addition of two numbers is :" + (num1 + num2));
+    }
+
+    //create static method to subtract two numbers
+    public static void subtractionOfTwoNumbers(int num1 , int num2){
+        System.out.println("Subtraction of two numbers is :" + (num1 - num2));
+    }
+}
---
+++ Ex1/DemoApp.java
@@ -0,0 +1,7 @@
+public class DemoApp {
+    public static void main(String[] args) {
+        //directly without creating objects.
+        Calculations.additionOfTwoNumbers(2,1);
+        Calculations.subtractionOfTwoNumbers(2,1);
+    }
+}
