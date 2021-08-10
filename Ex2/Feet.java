b018ab32ede43c89024680e351bf511fe4025f4f
MadhushaPrasad <madushaprasad21@gmail.com>
2021-08-10 17:35:00 +0530

    Implement an overloaded constructor that can accept another Feet object

--- Ex2/Feet.java
+++ Ex2/Feet.java
@@ -1,9 +1,23 @@
 class Feet {
  private int feet;
  private int inches;
- public Feet(int feet, int inches){}
+
+ public Feet(int feet, int inches){
+     this.feet = feet;
+     this.inches = inches;
+ }
+
+ public Feet(Feet len) {
+     
+ } 
+
 // Add f1+f2 feet and store in current feet
  public void add(Feet f1, Feet f2){}
-// Display a Length e.g 5’6” 
-public void print() {} 
+// Display a Length e.g 5ï¿½6ï¿½ 
+ public void print() {} 
+
+ //overLoad the print method
+ public void print(String msg) {
+     System.out.println(msg + " " + feet + "'" + inches + "'");
+ }
 }
---
+++ Ex2/FeetTest.java
@@ -0,0 +1,8 @@
+class FeetTest {
+    public static void main(String[] args) {
+        Feet mylength = new Feet(5,6);
+        mylength.print("Length : "); // should print Length : 5'6
+
+    }
+}
+   
