5c6e765d4ccd4a8bd462575eeff01a95694d08af
MadhushaPrasad <madushaprasad21@gmail.com>
2021-08-10 18:49:57 +0530

    exercise 02 done

--- Ex2/Feet.java
+++ Ex2/Feet.java
@@ -1,6 +1,7 @@
 class Feet {
  private int feet;
  private int inches;
+ private Feet feetOb;
 
  public Feet(int feet, int inches){
      this.feet = feet;
@@ -8,16 +9,36 @@ class Feet {
  }
 
  public Feet(Feet len) {
-     
+     this.feetOb = len;
  } 
 
 // Add f1+f2 feet and store in current feet
  public void add(Feet f1, Feet f2){}
+
 // Display a Length e.g 5�6� 
- public void print() {} 
+ public void print() {
+    feet += feetOb.feet;
+    inches += feetOb.inches;
+    feet += inches/12; 
+    inches %= 12;
+    System.out.println(feet + "'" + inches + "\"");
+ } 
 
  //overLoad the print method
  public void print(String msg) {
-     System.out.println(msg + " " + feet + "'" + inches + "'");
+     System.out.println(msg + " " + feet + "'" + inches + "\"");
  }
+
+ public void add(Feet f1) {
+     this.feetOb = f1;
+ }
+
+ public static void print(Feet len){
+     System.out.println(len.feet + "'" + len.inches + "\"");
+ }
+
+ public static Feet add(Feet f1, Feet f2, Feet f3) {
+     return new Feet(f1.feet + f2.feet + f3.feet , f1.inches + f2.inches + f3.inches);
+ }
+
 }
--- Ex2/FeetTest.java
+++ Ex2/FeetTest.java
@@ -1,8 +1,23 @@
 class FeetTest {
     public static void main(String[] args) {
-        Feet mylength = new Feet(5,6);
-        mylength.print("Length : "); // should print Length : 5'6
+    
+    //q->ii.
+    Feet mylength = new Feet(5,6);
+    mylength.print("Length : "); // should print Length : 5'6"
 
+    //q->iv.
+	Feet newlen = new Feet(6,7);
+	mylength.add(newlen);
+	mylength.print(); // 12'1"
+
+    //q->v
+    Feet.print(mylength);
+
+    //q->vii
+    Feet f1 = new Feet(1,2);
+    Feet f2 = new Feet(3,4);
+    Feet f3 = new Feet(5,6);
+    Feet f5 = Feet.add(f1, f2, f3);
     }
 }
    
